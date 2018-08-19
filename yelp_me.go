892558aed9bf450f27059673975f48194781f948
rush <rush.8733@gmail.com>
2018-08-19 01:05:10 -0400

    ran go fmt on the file -RS

--- yelp_me.go
+++ yelp_me.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os"
 	gabs "github.com/Jeffail/gabs"
 	pflag "github.com/spf13/pflag"
 	viper "github.com/spf13/viper"
 	"gopkg.in/resty.v1"
+	"os"
 	"strconv"
 )
 
@@ -18,10 +18,10 @@ func ReadConfig() (string, string, bool) {
 	err := viper.ReadInConfig() // Find and read the config file
 	if err != nil {             // Handle errors reading the config file
 		fatalStr := "Fatal error config file: %s \n" +
-          "Place config file in $HOME/.go_grub.yml\n" + 
-          "yelp: \n  api_url: apiurlgoeshere (not required has default set)\n" +
-          "  api_token: apitokegoeshere \n" +
-					"debug: true (not required)"
+			"Place config file in $HOME/.go_grub.yml\n" +
+			"yelp: \n  api_url: apiurlgoeshere (not required has default set)\n" +
+			"  api_token: apitokegoeshere \n" +
+			"debug: true (not required)"
 		panic(fmt.Errorf(fatalStr, err))
 	}
 	viper.SetDefault("yelp.api_url", "https://api.yelp.com/v3/")
@@ -73,36 +73,36 @@ func RestyConfig(yelpAPIUrl string, yelpAPIToken string, debug bool) {
 func RequestBuisnessSearch(keywordSearch string, zipSearch int, distanceSearch int) []byte {
 	resp, err := resty.R().
 		SetQueryParams(map[string]string{
-			"term":     keywordSearch,
-			"location": strconv.Itoa(zipSearch),
-			"radius":   strconv.Itoa(distanceSearch),
-      "categories": "restaurants",
-			"open_now": "true",
-			"sort_by":  "rating",
+			"term":       keywordSearch,
+			"location":   strconv.Itoa(zipSearch),
+			"radius":     strconv.Itoa(distanceSearch),
+			"categories": "restaurants",
+			"open_now":   "true",
+			"sort_by":    "rating",
 		}).
 		Get("businesses/search")
 	//fmt.Printf("\nResponse Body: %v", resp.String())
 	if err != nil {
 		fmt.Printf("\nResponse Err: %v", err)
 	}
-  return resp.Body()
+	return resp.Body()
 }
 
 func ParseResponse(input []byte) {
-  jsonParsed, err := gabs.ParseJSON(input)
-  if err != nil {
+	jsonParsed, err := gabs.ParseJSON(input)
+	if err != nil {
 		fmt.Printf("\ngabs.ParseJson err: %v", err)
-  }
-  children, _ := jsonParsed.S("businesses").Children()
-  for _, child := range children {
-	  fmt.Println("| Name:", child.Search("name").Data(), "| Review count: ", child.Search("review_count"), "| Rating:",  child.Search("rating").Data(), "| Price: ", child.Search("price"), "|")
-  }
+	}
+	children, _ := jsonParsed.S("businesses").Children()
+	for _, child := range children {
+		fmt.Println("| Name:", child.Search("name").Data(), "| Review count: ", child.Search("review_count"), "| Rating:", child.Search("rating").Data(), "| Price: ", child.Search("price"), "|")
+	}
 }
 
 func main() {
-  yelpAPIUrl, yelpAPIToken, debug := ReadConfig()
-  keywordSearch, zipSearch, distanceSearch := ParseCLI(debug)
-  RestyConfig(yelpAPIUrl, yelpAPIToken, debug)
-  buisnessBody := RequestBuisnessSearch(keywordSearch, zipSearch, distanceSearch)
-  ParseResponse(buisnessBody)
+	yelpAPIUrl, yelpAPIToken, debug := ReadConfig()
+	keywordSearch, zipSearch, distanceSearch := ParseCLI(debug)
+	RestyConfig(yelpAPIUrl, yelpAPIToken, debug)
+	buisnessBody := RequestBuisnessSearch(keywordSearch, zipSearch, distanceSearch)
+	ParseResponse(buisnessBody)
 }
